87bcd465bebaf82d09148fe5621246e5690585d1
altairantares <altairantares09@gmail.com>
2024-09-04 13:47:03 +0700

    index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!-- 
+Warning: Undefined variable $res in D:\xampp\htdocs\Surat\index.php on line 151
+
+Warning: Trying to access array offset on value of type null in D:\xampp\htdocs\Surat\index.php on line 151 
+
+Fatal error: Uncaught mysqli_sql_exception: Table 'surat.libur' doesn't exist in D:\xampp\htdocs\Surat\showmatriks.php:20 Stack trace: #0 D:\xampp\htdocs\Surat\showmatriks.php(20): mysqli_query(Object(mysqli), 'SELECT * FROM l...') #1 {main} thrown in D:\xampp\htdocs\Surat\showmatriks.php on line 20
+
+-->
+
 <?php
 session_start();
 include_once("db_connection.php");
@@ -41,53 +50,7 @@ if ($_SESSION['level'] == "Administrator") {
     ?>
 
     <main>
-        <!-- <table>
-      <tr style='color:#fff;'>
-         <td align='center' bgcolor='#3db2e1'>Tahun</td>
-         <td align='center' bgcolor='#3db2e1'>Bulan</td>
-         <td align='center' bgcolor='#3db2e1'>Pegawai</td>
-         <td align='center' rowspan='2'><button id="proses" name="proses" onclick="cari();">Tampil</button></td>
-         <td align='center' rowspan='2'><button id="excel" name="excel" onclick="excel();">Excel</button></td>
-      </tr>
-      <tr>
-         <td>
-            <select id="tahun" name="tahun">
-               <option selected>--Pilih--</option>
-               <option value="2024">2024</option>
-            </select>
-         </td>
-         <td>
-            <select id="bulan" name="bulan">
-               <option selected>--Pilih--</option>
-               <option value="01">Januari</option>
-               <option value="1">Februari</option>
-                <option value="2">Maret</option>
-                <option value="3">April</option>
-                <option value="4">Mei</option>
-                <option value="5">Juni</option>
-                <option value="6">Juli</option>
-                <option value="7">Agustus</option>
-                <option value="8">September</option>
-                <option value="9">Oktober</option>
-                <option value="10">November</option>
-                <option value="11">Desember</option>
-            </select>
-         </td>
-         <td>
-            <select id="nip" name="nip">
-               <option selected>--Pilih--</option>
-               <?php
-                if ($_SESSION['level'] == "Administrator") {
-                    echo '<option value="all">Semua Pegawai</option>';
-                }
-                while ($res = mysqli_fetch_array($result)) {
-                    echo "<option value=" . $res['nip'] . ">" . $res['nama'] . "</option>";
-                }
-                ?>
-            </select>
-         </td>
-      </tr>
-   </table>
+        <!-- 
    <br>
    <span id="data"></span>
    <br>
@@ -119,6 +82,63 @@ if ($_SESSION['level'] == "Administrator") {
    }
    </script> -->
 
+
+        <div>
+            <?php if ($_SESSION['level'] == "Administrator") { ?>
+                <table>
+                    <tr style='color:#fff;'>
+                        <td align='center' bgcolor='#3db2e1'>Tahun</td>
+                        <td align='center' bgcolor='#3db2e1'>Bulan</td>
+                        <td align='center' bgcolor='#3db2e1'>Pegawai</td>
+                        <td align='center' rowspan='2'><button id="proses" name="proses" onclick="cari();">Tampil</button></td>
+                        <td align='center' rowspan='2'><button id="excel" name="excel" onclick="excel();">Excel</button></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select id="tahun" name="tahun">
+                                <option selected>--Pilih--</option>
+                                <option value="2024">2024</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select id="bulan" name="bulan">
+                                <option selected>--Pilih--</option>
+                                <option value="01">Januari</option>
+                                <option value="1">Februari</option>
+                                <option value="2">Maret</option>
+                                <option value="3">April</option>
+                                <option value="4">Mei</option>
+                                <option value="5">Juni</option>
+                                <option value="6">Juli</option>
+                                <option value="7">Agustus</option>
+                                <option value="8">September</option>
+                                <option value="9">Oktober</option>
+                                <option value="10">November</option>
+                                <option value="11">Desember</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select id="nip" name="nip">
+                                <option selected>--Pilih--</option>
+                                <?php
+                                if ($_SESSION['level'] == "Administrator") {
+                                    echo '<option value="all">Semua Pegawai</option>';
+                                }
+                                while ($res = mysqli_fetch_array($result)) {
+                                    echo "<option value=" . $res['nip'] . ">" . $res['nama'] . "</option>";
+                                }
+                                ?>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+                <br>
+                <span id="data"></span>
+                <br>
+                <ul>.</ul>
+        </div>
+    <?php } else { ?>
+        <div><?php echo $_SESSION["user"]; ?></div>
         <div class="filter">
             <label for="month-select">Bulan</label>
             <select id="month-select" onchange="generateCalendar()">
@@ -135,32 +155,15 @@ if ($_SESSION['level'] == "Administrator") {
                 <option value="10">November</option>
                 <option value="11">Desember</option>
             </select>
+        </div>
+        <div class="employee-name"> 
+            <?php echo ''.$res['nama'] .''?>
+        </div>
+        <div class="calendar">
+            <div class="days" id="days-container">
             </div>
-            <div class="employee-name">
-                <?php if ($_SESSION['level'] == "Administrator") { ?>
-                    <select id="nip" name="nip">
-                        <option selected>--Pilih--</option>
-                        <?php
-                        if ($_SESSION['level'] == "Administrator") {
-                            echo '<option value="all">Semua Pegawai</option>';
-                        }
-                        while ($res = mysqli_fetch_array($result)) {
-                            echo "<option value=" . $res['nip'] . ">" . $res['nama'] . "</option>";
-                        }
-                        ?>
-                    </select>
-                    <br>
-                    <span id="data"></span>
-                    <br>
-                    <ul>.</ul>
-                    </div>
-                <?php } else { ?>
-                    <div><?php echo $_SESSION["user"]; ?></div>
-                    <div class="calendar">
-                        <div class="days" id="days-container">
-                        </div>
-                    </div>
-                <?php } ?>
+        </div>
+    <?php } ?>
     </main>
     <script>
         function cari() {
