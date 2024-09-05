5cffbf9149616ce62a796170e0b676ce2560c8d2
altrantrs <99790188+altrantrs@users.noreply.github.com>
2024-09-05 09:59:33 +0700

    Update showmatriks.php

--- showmatriks.php
+++ showmatriks.php
@@ -1,20 +1,4 @@
-<!-- Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92
-
-Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92
-
-Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92
-
-Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92
-
-Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92
-
-Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92
-
-Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92
-
-Warning: Array to string conversion in D:\xampp\htdocs\Surat\showmatriks.php on line 92 -->
 <?php
-
 session_start();
 include_once("db_connection.php");
 
@@ -24,37 +8,39 @@ if (!isset($_SESSION['user'])) {
     exit;
 }
 
-// Pastikan 'tahun' dan 'bulan' ada dan valid
+// Validate 'tahun' and 'bulan'
 $tahun = isset($_REQUEST['tahun']) ? $_REQUEST['tahun'] : date('Y');
 $bulan = isset($_REQUEST['bulan']) ? $_REQUEST['bulan'] : date('m');
 $nip = isset($_REQUEST['nip']) ? $_REQUEST['nip'] : '';
 
-// Validasi input tahun dan bulan
 if (!preg_match('/^\d{4}$/', $tahun) || !preg_match('/^(0[1-9]|1[0-2])$/', $bulan)) {
     die("Tahun atau bulan tidak valid.");
 }
 
-// Tentukan tanggal awal dan akhir bulan
+// Set start and end dates for the month
 $tgl_awal = "$tahun/$bulan/01";
 $awal_tgl = strtotime($tgl_awal);
 $tgl_akhir = "$tahun/$bulan/" . date("t", $awal_tgl);
 
-// Query pegawai berdasarkan NIP
+// Query pegawai
 $sql_peg = ($nip == "all") ? "SELECT * FROM pegawai ORDER BY nama" : "SELECT * FROM pegawai WHERE nip='$nip' ORDER BY nama";
 $result = mysqli_query($conn, $sql_peg);
 
-// Tampilkan tabel kegiatan
+// Display the table
 echo "<table border='1' cellspacing='0' cellpadding='5'>
 <tr bgcolor='#FFFCCC'>
 <td align='center' colspan='40'>Kegiatan di Bulan " . bulan($bulan) . " $tahun</td></tr>";
 
-// Loop setiap pegawai dan tampilkan hari kerja
+// Loop through employees
 while ($pegawai = mysqli_fetch_array($result)) {
     $nama = isset($pegawai['nama']) ? $pegawai['nama'] : '';
     echo tampilkanHariKerja($pegawai['nip'], $bulan, $tahun, $tgl_awal, $tgl_akhir, $nama);
 }
 
-// Fungsi menampilkan hari kerja
+// Display the closing tag for the table
+echo "</table>";
+
+// Function to display working days
 function tampilkanHariKerja($nip, $bulan, $tahun, $tgl_awal, $tgl_akhir, $nama) {
     global $conn;
     $awal_tgl = strtotime($tgl_awal);
@@ -62,7 +48,7 @@ function tampilkanHariKerja($nip, $bulan, $tahun, $tgl_awal, $tgl_akhir, $nama) 
 
     echo "<tr><td>$nama</td>";
 
-    // Loop setiap tanggal dalam bulan tersebut
+    // Loop through each date in the month
     for ($waktu = $awal_tgl; $waktu <= $akhir_tgl; $waktu = strtotime("+1 day", $waktu)) {
         $current_date = date("Y-m-d", $waktu);
         $hari_temp = date("d", $waktu);
@@ -72,18 +58,17 @@ function tampilkanHariKerja($nip, $bulan, $tahun, $tgl_awal, $tgl_akhir, $nama) 
         $sql_activity = "SELECT * FROM activity_dates WHERE created_by='$nip' AND date='$current_date'";
         $result_activity = mysqli_query($conn, $sql_activity);
 
-        // Jika ada kegiatan di hari itu
         if ($result_activity && mysqli_num_rows($result_activity) > 0) {
             $activity = mysqli_fetch_array($result_activity, MYSQLI_ASSOC);
             $activity_info = getActivityInfo(isset($activity['activity_id']) ? $activity['activity_id'] : '');
 
-            echo getActivityCell($activity_info, $bln_temp, $hari_temp, $bg_color, $nip);
+            echo getActivityCell($activity_info, $bln_temp, $current_date, $bg_color);
         } else {
             echo getEmptyCell($bg_color, $bln_temp, $hari_temp, $nip);
         }
     }
 
-    // Rekap jumlah kegiatan pegawai per bulan
+    // Summary of activities
     $rekap = mysqli_query($conn, "SELECT COUNT(activity_id) as jum FROM activity_dates WHERE created_by='$nip' AND MONTH(date)='$bulan' AND YEAR(date)='$tahun'");
     $rekap_data = mysqli_fetch_array($rekap, MYSQLI_ASSOC);
     $rekap_color = ($rekap_data['jum'] > 15) ? "red" : (($rekap_data['jum'] > 0) ? "green" : "white");
@@ -91,24 +76,21 @@ function tampilkanHariKerja($nip, $bulan, $tahun, $tgl_awal, $tgl_akhir, $nama) 
     echo "<td bgcolor='$rekap_color'><p style='color:white;'>" . (isset($rekap_data['jum']) ? $rekap_data['jum'] : '0') . "</p></td></tr>";
 }
 
-// Fungsi mengkonversi hari menjadi inisial
+// Convert day to initial
 function convertDayToInitial($day) {
     $days = ["Mon" => "S", "Tue" => "S", "Wed" => "R", "Thu" => "K", "Fri" => "J", "Sat" => "S", "Sun" => "M"];
     return isset($days[$day]) ? $days[$day] : '';
 }
 
-// Fungsi menampilkan cell kegiatan
-function getActivityCell($bln_temp, $current_date, $bg_color) {
-    
-    $activity_button = "<button onclick=\"window.location.href='perjalanan_tambah.php?tanggal=$current_date';\" </button>";
-    // $delete_icon = "<i class='fas fa-trash-alt' title='Hapus' onclick=\"deleteActivity()\"></i>";
+// Display activity cell
+function getActivityCell($activity_info, $bln_temp, $current_date, $bg_color) {
+    $activity_button = "<button onclick=\"window.location.href='perjalanan_tambah.php?tanggal=$current_date';\">Add Activity</button>";
     $print_icon = "<i class='fa-solid fa-check' title='Print' onclick=\"window.location.href='tampil_kegiatan.php?date=$current_date';\"></i>";
-    
-    return "<td align='center' bgcolor='$bg_color'>$bln_temp<br>$activity_button $print_icon</td>";
+
+    return "<td align='center' bgcolor='$bg_color'>$bln_temp<br>$activity_button<br>$print_icon</td>";
 }
 
-
-// Fungsi menampilkan cell kosong
+// Display empty cell
 function getEmptyCell($bg_color, $bln_temp, $hari_temp, $nip) {
     if ($_SESSION['level'] == "Administrator") {
         return "<td align='center' bgcolor='$bg_color'>$bln_temp<br><button onclick=\"document.getElementById('id01').style.display='block';isi('$nip','$hari_temp');\" class='tanggal0'>$hari_temp</button></td>";
@@ -116,13 +98,13 @@ function getEmptyCell($bg_color, $bln_temp, $hari_temp, $nip) {
     return "<td align='center' bgcolor='$bg_color'>$bln_temp<br><button class='tanggal0'>$hari_temp</button></td>";
 }
 
-// Fungsi mengkonversi bulan
+// Get month name
 function bulan($bln) {
     $bulan_array = ["01" => "Januari", "02" => "Februari", "03" => "Maret", "04" => "April", "05" => "Mei", "06" => "Juni", "07" => "Juli", "08" => "Agustus", "09" => "September", "10" => "Oktober", "11" => "November", "12" => "Desember"];
     return isset($bulan_array[$bln]) ? $bulan_array[$bln] : '';
 }
 
-// Fungsi untuk mendapatkan info kegiatan
+// Get activity info
 function getActivityInfo($activity_id) {
     global $conn;
     $sql = "SELECT * FROM activities WHERE id='$activity_id'";
