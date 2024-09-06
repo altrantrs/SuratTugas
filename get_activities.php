101ec79253794a23992d47397f634608753814ab
altairantares <altairantares09@gmail.com>
2024-09-06 14:44:46 +0700

    pelaksana

--- get_activities.php
+++ get_activities.php
@@ -8,35 +8,27 @@ $pelaksana_name = isset($_GET['pelaksana']) ? $_GET['pelaksana'] : '';
 
 if ($_SESSION['level'] == "Administrator") {
     if ($pelaksana_name === "all") {
-        $query = "SELECT activity_dates.date, activity_dates.pelaksana 
+        $query = "SELECT activity_dates.date, pegawai.nama 
                   FROM activity_dates
+                  JOIN pegawai ON activity_dates.pelaksana = pegawai.nama
                   WHERE MONTH(activity_dates.date) = $month AND YEAR(activity_dates.date) = $year";
     } else {
-        $query_nip = "SELECT nama FROM pegawai WHERE nama='$pelaksana_name'";
-        $result_nip = mysqli_query($conn, $query_nip);
-        $nip_row = mysqli_fetch_assoc($result_nip);
-        $pelaksana_nip = $nip_row['nama'];
-
-        $query = "SELECT activity_dates.date, activity_dates.pelaksana 
+        $query = "SELECT activity_dates.date, pegawai.nama 
                   FROM activity_dates
-                  WHERE MONTH(activity_dates.date) = $month AND YEAR(activity_dates.date) = $year AND activity_dates.pelaksana='$pelaksana_nip'";
+                  JOIN pegawai ON activity_dates.pelaksana = pegawai.nama
+                  WHERE MONTH(activity_dates.date) = $month AND YEAR(activity_dates.date) = $year AND pegawai.nama='$pelaksana_name'";
     }
 } else {
-    $query_nip = "SELECT nama FROM pegawai WHERE nama='$pelaksana_name'";
-    $result_nip = mysqli_query($conn, $query_nip);
-    $nip_row = mysqli_fetch_assoc($result_nip);
-    $pelaksana_nip = $nip_row['nama'];
-
-    $query = "SELECT activity_dates.date, activity_dates.pelaksana 
-              FROM activity_dates 
-              WHERE MONTH(activity_dates.date) = $month AND YEAR(activity_dates.date) = $year AND pelaksana='$pelaksana_nip'";
+    $query = "SELECT activity_dates.date, pegawai.nama 
+              FROM activity_dates
+              JOIN pegawai ON activity_dates.pelaksana = pegawai.nama
+              WHERE MONTH(activity_dates.date) = $month AND YEAR(activity_dates.date) = $year AND pegawai.nama='$pelaksana_name'";
 }
 
 
 $result = mysqli_query($conn, $query);
 
 if (!$result) {
-    // Handle query error
     echo json_encode(['error' => mysqli_error($conn)]);
     exit;
 }
@@ -46,7 +38,7 @@ $activities = [];
 while ($row = mysqli_fetch_assoc($result)) {
     $activities[] = [
         'date' => $row['date'],
-        'pelaksana' => $row['pelaksana'] // Menyertakan kolom pelaksana
+        'pelaksana' => $row['nama'] // Returning the name instead of NIP
     ];
 }
 
